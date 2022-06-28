d23c827777bfa7eca3b1512ce17f76a91ec33ba9
learner-22 <nbhooma@gmail.com>
2022-06-28 12:26:40 -0400

    Added form to get data

--- src/App.js
+++ src/App.js
@@ -8,11 +8,56 @@ import TodosList from './components/TodosList'
 
 
 class App extends Component {
- render(){
+ 
+ state = {
+  todosData : todosData,
+  task: '',
+  details :"",
+  completed : "false",
+  createdAt : ""
+ }
+
+ handleChange = (event) =>{
+  this.setState({[event.target.id] : event.target.value} )
+ }
+
+ handleSubmit = (event)=>{
+   event.preventDefault()
+   const newTodo  = {
+    task: this.state.task,
+  details :this.state.details,
+  completed : this.state.completed,
+  createdAt :this.state.createdAt
+   }
+   console.log(newTodo)
+   this.setState({
+    todosData : [newTodo, ...this.state.todosData],
+  task: '',
+  details :"",
+  completed : "false",
+  createdAt : ""
+   })
+ }
+  render(){
   return(
     <div className="App">
       <Navbar />
-      <TodosList todos={todosData} />
+      <form onSubmit = {this.handleSubmit}>
+      <label htmlFor = "task"> ToDo Task </label>
+      <input type ="text" value ={this.state.task} onChange ={this.handleChange} id="task" />
+      <br />
+      <label htmlFor = "details"> Task Details</label>
+      <input type ="text" value ={this.state.details} onChange ={this.handleChange} id="details" />
+      <br />
+      <label htmlFor = "completed"> Task Status </label>
+      <input type ="text" value ={this.state.completed} onChange ={this.handleChange} id="completed" />
+      <br/>
+      <label htmlFor = "createdAt"> Task Created At </label>
+      <input type ="text" value ={this.state.createdAt} onChange ={this.handleChange} id="createdAt" />
+      <br/>
+      <input type="submit" />
+      </form>
+      <TodosList todos={this.state.todosData} />
     </div>
   )
  }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 const Navbar = () =>{
     return(
         <nav style ={styles.navStyle}>
-            <h1> Todos App</h1>
+           <h1> Todos App</h1>
             <ul>
                 <li>Home</li>
                 <li>ToDos</li>
                 <li> About</li>
             </ul>
+            
         </nav>
     )
 }
